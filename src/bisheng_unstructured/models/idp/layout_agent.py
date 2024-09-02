fc5891065cb4d50d60389c1744d04d7548293e7b
yaojin <yaojin@dataelem.com>
2024-09-02 15:01:04 +0800

    support sdk

--- src/bisheng_unstructured/models/idp/layout_agent.py
+++ src/bisheng_unstructured/models/idp/layout_agent.py
@@ -1,6 +1,6 @@
-import copy
-
-import requests
+import json
+import numpy as np
+import tritonclient.http as httpclient
 
 
 # Layout Agent Version 0.1, update at 2023.08.18
@@ -8,20 +8,23 @@ class LayoutAgent(object):
 
     def __init__(self, *args, **kwargs):
         self.ep = kwargs.get("layout_ep")
-        self.client = requests.Session()
+        ep_parts = self.ep.split("/")
+        self.model = ep_parts[-2]
+        server_url = ep_parts[2]
+        self.client = httpclient.InferenceServerClient(url=server_url, verbose=False)
         self.timeout = kwargs.get("timeout", 60)
         self.params = {
             "longer_edge_size": 0,
         }
 
     def predict(self, inp):
-        params = copy.deepcopy(self.params)
-        params.update(inp)
-        # print('params', params, self.ep)
+        input0_data = np.asarray([json.dumps(inp)], dtype=np.object_)
+        inputs = [httpclient.InferInput("INPUT", [1], "BYTES")]
+        inputs[0].set_data_from_numpy(input0_data)
+        outputs = [httpclient.InferRequestedOutput("OUTPUT")]
         try:
-            r = self.client.post(url=self.ep, json=params, timeout=self.timeout)
-            return r.json()
-        except requests.exceptions.Timeout:
-            raise Exception(f"timeout in layout predict")
+            response = self.client.infer(self.model, inputs, request_id=str(1), outputs=outputs)
+            output_data = json.loads(response.as_numpy("OUTPUT")[0].decode("utf-8"))
         except Exception as e:
             raise Exception(f"exception in layout predict: [{e}]")
+        return output_data
